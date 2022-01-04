0c346e04d23751782efc0ccaface1f81a79f821f
Waleed <waleedthedev@gmail.com>
2022-01-04 18:35:11 +0500

    Developed mobile layout, added small animations using Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,15 @@
 <script>
+  import { fade } from 'svelte/transition'
+
+  let emailIsValid = true
+
+  const validEmailCheck = (e) => {
+    // console.log('Here!')
+    const email = e.target.email.value
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    emailIsValid = re.test(email) ? true : false
+  }
 </script>
 
 <svelte:head>
@@ -20,7 +31,7 @@
       <img src="/assets/logo.svg" alt="logo" />
     </div>
     <img src="/assets/hero-mobile.jpg" alt="hero" class="w-full" />
-    <div class="max-w-sm mx-auto">
+    <div class="max-w-sm mx-auto px-5">
       <h1 class="uppercase text-5xl text-center pt-20 pb-6 tracking-widest">
         <span class="text-cstm-primary-desaturated-red font-light"
           >We're<br /></span
@@ -32,14 +43,38 @@
         launch deals.
       </p>
     </div>
-    <div>
-      Email Address
-      <input type="email" />
-      <img
-        src="/assets/icon-arrow.svg"
-        alt="right arrow"
-        class="bg-pink-to-orangish px-8 py-5 rounded-full"
-      />
+    <div class="max-w-sm mx-auto px-5 my-10">
+      <form
+        on:submit|preventDefault={validEmailCheck}
+        class="flex items-center mb-2 max-w-sm mx-auto border-2 border-cstm-primary-desaturated-red rounded-full"
+      >
+        <input
+          type="text"
+          name="email"
+          placeholder="Email Address"
+          class="w-full rounded-full p-5 placeholder:text-cstm-primary-desaturated-red focus:outline-none"
+        />
+        {#if !emailIsValid}
+          <img
+            transition:fade
+            src="/assets/icon-error.svg"
+            alt="error icon"
+            class="pr-5"
+          />
+        {/if}
+        <button
+          type="submit"
+          value="Submit"
+          class="bg-pink-to-orangish hover:scale-110 hover:brightness-125 transition-all duration-200 pl-11 pr-9 py-5 rounded-full"
+        >
+          <img src="/assets/icon-arrow.svg" alt="right arrow" width="24" />
+        </button>
+      </form>
+      {#if !emailIsValid}
+        <p transition:fade class="text-red-500 ml-6">
+          Please provide a valid email
+        </p>
+      {/if}
     </div>
   </div>
 </main>
